f7fb089498d4e414513b4eb3a276236f4efefbfb
seanj2776 <seanj2776@student.faytechcc.edu>
2024-09-22 16:00:01 -0400

    Add files via upload

---
+++ P1LAB1_Senn Joseph.py
@@ -0,0 +1,13 @@
+#Joseph Senn
+#9/22/2024
+#P1LAB1
+#This assignmnet prompts you to write a code asking the person for their first name and last name, followed by a greeting message.
+
+# Prompt the user for their first name
+first_name = input('Enter your first name: ')
+
+# Prompt the user for their last name
+last_name = input('Enter your last name: ')
+
+# Print the greeting message
+print('Hello,', first_name, last_name + '! Welcome to CTI-110')
